8694e389a9be4080c67ed14be9dc5c98a234ada1
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-07-31 15:19:35 +0600

    :partly_sunny: MODIFIED spree route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   #
   # We ask that you don't use the :as option here, as Spree relies on it being
   # the default of "spree".
-  mount Spree::Core::Engine, at: '/'
+  mount Spree::Core::Engine, at: '/api'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
