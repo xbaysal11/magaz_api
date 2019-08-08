be9145f899762ceb1ceb2d32ed5752e190bee925
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-08-08 20:15:56 +0600

    :seedling: ADDED cors delete req

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module MagazApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :options]
       end
     end
     config.to_prepare do
